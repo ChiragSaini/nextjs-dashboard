f03364b4db91ff9cca9bee170a2018be2ca0f80f
Chirag Saini <chiragsaini7777@gmail.com>
2024-05-01 16:24:27 +0530

    Done upto chapter 11

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-function Page() {
-  return (
-    <div>Dashboard Page</div>
-  )
-}
-
-export default Page;
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,21 +9,31 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
+
+function randomIntFromInterval(min: number, max: number) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const rndInt = randomIntFromInterval(1, 6);
+
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
+    const randomTimer = randomIntFromInterval(1,3)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, randomTimer*1000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log(`Data fetch completed after ${randomTimer} seconds.`);
 
     return data.rows;
   } catch (error) {
@@ -33,13 +43,24 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore();
+
   try {
+
+    const randomTimer = randomIntFromInterval(1, 3)
+
+    console.log('Fetching invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, randomTimer*1000));
+
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log(`Data fetch completed after ${randomTimer} seconds.`);
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
@@ -53,6 +74,8 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore();
+
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -64,11 +87,18 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+    const randomTimer = randomIntFromInterval(1, 3)
+
+    console.log('Fetching invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, randomTimer*1000));
+
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
     ]);
+
+    console.log(`Data fetch completed after ${randomTimer} seconds.`);
 
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
@@ -92,7 +122,10 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  noStore();
+
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+  console.log(`Triggered with query: ${query}`);
 
   try {
     const invoices = await sql<InvoicesTable>`
@@ -124,6 +157,8 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore();
+
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -145,6 +180,8 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -170,6 +207,8 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  noStore();
+
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -188,6 +227,8 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore();
+
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -221,6 +262,8 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
+  noStore();
+
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
